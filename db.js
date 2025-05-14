e4fe5a75251959b37bfd9fb5afd3068fb8f320f7
Иван Ефремов <efremov.ivan04-06-2004@yandex.ru>
2025-05-14 13:53:22 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('notifications_db', 'root', 'Ef89211991234', {
+const sequelize = new Sequelize('notifications_db', 'root', 'root', {
   host: 'localhost',
   dialect: 'mysql',
   logging: false,
